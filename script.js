8faedae97896b34614d3ee2a8b338a6012322c2c
Billy Dorman <b.d.dorman12@gmail.com>
2021-05-10 20:18:23 -0500

    assigned variables in script file

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//assigning variables for password generator
+var selectPasswordLength;
+var selectLowerCase;
+var selectUpperCase;
+var selectNumber;
+var selectSpecialChar;
+
+//creating arrays with variables
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
